a7e947224c37d3a252f411df18e2736a41a6ee62
Max Hager <maxhager28@gmail.com>
2024-12-10 00:34:11 +0800

    change

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   rewrites: async () => {
     return [
@@ -8,31 +6,9 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
-            : "https://api.stablecharacter.com/api/py/:path*",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
-      },
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.stablecharacter.com',
+            : "/api/py/:path*",  // Keep it relative in production
       }
-    ],
+    ];
   }
 };
 
